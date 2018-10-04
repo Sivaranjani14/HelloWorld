db090a27f9ad30a22d3485ebf5a8ccc784b25b37
Selvaraj <sivaranjani.selvaraj@accenture.com>
2018-10-04 15:44:20 +0530

    modified the test casessss

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -24,21 +24,25 @@ public class HelloTest {
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/Helloworld.war/");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);*/
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+		*/
 	}
 
 	@Test
 	public void f() {
-                /*
+        /*
 		text = driver.findElement(By.xpath("html/body/h2"));
 		Assert.assertEquals(driver.getTitle(), "");
-		System.out.println("Test Passed");*/
+		System.out.println("Test Passed");
+		*/
 
 	}
 
 	@AfterTest
-	/*public void afterTest() {
-		//driver.close();*/
+	/*
+	 public void afterTest() {
+	 //driver.close();
+	*/
 	}
 
 
